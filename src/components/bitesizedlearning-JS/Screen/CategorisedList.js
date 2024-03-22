11634bdac5ce7667fa74f3f92a1e2ab04bca7940
itsLeonnnnn <leon.then.kch@gmail.com>
2024-03-23 05:19:45 +0800

    amended bitesizedlearning-category UI

--- src/components/bitesizedlearning-JS/Screen/CategorisedList.js
+++ src/components/bitesizedlearning-JS/Screen/CategorisedList.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { useRoute } from "@react-navigation/native";
 import { useEffect } from "react";
@@ -7,9 +7,11 @@ import { FlatList } from "react-native-gesture-handler";
 import CategorisedListItem from "./CategorisedListItem";
 import { useState } from "react";
 import { StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function CategorisedList({ category }) {
   const [courseList, setCourseList] = useState([]);
+  const navigation = useNavigation();
   useEffect(() => {
     getCategorisedList();
   }, [category]);
@@ -33,28 +35,99 @@ export default function CategorisedList({ category }) {
       >
         <Text style={{ fontSize: 20, fontFamily: "Inter-Bold" }}>Stocks</Text>
       </View>
-      <View style={styles.container}>
-        <Image
-          source={{ uri: "https://i.ibb.co/0FfYS2B/capital-market-1.png" }}
-          style={styles.image}
-        />
-        <View style={styles.subContainer}>
-          <Text style={{ fontFamily: "Inter-Bold", fontSize: 19 }}>
-            Stocks/Shares
-          </Text>
-          <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 16 }}>
-            Free
-          </Text>
-          <View style={{ flexDirection: "row", gap: 15 }}>
-            <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
-              📖 2 Chapter(s)
+      <TouchableOpacity
+      // onPress={() =>
+      //   navigation.navigate("course-detail", {
+      //     course: "Stocks/Shares",
+      //   })
+      // }
+      >
+        <View style={styles.container}>
+          <Image
+            source={{ uri: "https://i.ibb.co/0FfYS2B/capital-market-1.png" }}
+            style={styles.image}
+          />
+          <View style={styles.subContainer}>
+            <Text style={{ fontFamily: "Inter-Bold", fontSize: 19 }}>
+              Stocks/Shares
             </Text>
-            <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
-              ⏰ 1 hour(s)
-            </Text>
+            <View style={{ flexDirection: "row", gap: 65 }}>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                📊 Basic
+              </Text>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                💰 Free
+              </Text>
+            </View>
+            <View style={{ flexDirection: "row", gap: 15 }}>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                📖 2 Chapter(s)
+              </Text>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                ⏰ 1 hour(s)
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            source={{ uri: "https://i.ibb.co/3Bb0Cvg/capital-market-7.png" }}
+            style={styles.image}
+          />
+          <View style={styles.subContainer}>
+            <Text style={{ fontFamily: "Inter-Bold", fontSize: 19 }}>
+              Craft Stocks Portfolio
+            </Text>
+            <View style={{ flexDirection: "row", gap: 34 }}>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                📊 Moderate
+              </Text>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                💰 Free
+              </Text>
+            </View>
+            <View style={{ flexDirection: "row", gap: 15 }}>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                📖 2 Chapter(s)
+              </Text>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                ⏰ 1 hour(s)
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            source={{ uri: "https://i.ibb.co/x5WNY3M/capital-market-8.png" }}
+            style={styles.image}
+          />
+          <View style={styles.subContainer}>
+            <Text style={{ fontFamily: "Inter-Bold", fontSize: 19 }}>
+              Strategies in Stocks
+            </Text>
+            <View style={{ flexDirection: "row", gap: 31 }}>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                📊 Advanced
+              </Text>
+              <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 13 }}>
+                💰 Free
+              </Text>
+            </View>
+            <View style={{ flexDirection: "row", gap: 15 }}>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                📖 2 Chapter(s)
+              </Text>
+              <Text style={{ fontFamily: "Inter-Regular", fontSize: 13 }}>
+                ⏰ 1 hour(s)
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
